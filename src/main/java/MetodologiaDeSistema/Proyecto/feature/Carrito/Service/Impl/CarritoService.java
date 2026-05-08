0e755c26f3d4069bc61fcf1a543a2f2fc3750d81
RamiroHH <ramiroh.dev@gmail.com>
2026-05-07 23:25:08 -0300

    Create CarritoService.java

---
+++ src/main/java/MetodologiaDeSistema/Proyecto/feature/Carrito/Service/Impl/CarritoService.java
@@ -0,0 +1,10 @@
+package MetodologiaDeSistema.Proyecto.feature.Carrito.Service.Impl;
+
+public interface CarritoService {
+
+    void agregarProducto(Long carritoId, Long productoId, Integer cantidad);
+
+    void eliminarItem(Long itemId);
+
+    void modificarCantidad(Long itemId, Integer cantidad);
+}
