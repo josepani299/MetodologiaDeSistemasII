c5dfda62b3030cba6917629e00770645965f84c1
RamiroHH <ramiroh.dev@gmail.com>
2026-05-07 23:24:52 -0300

    Create ModificarCantidadController.java

---
+++ src/main/java/MetodologiaDeSistema/Proyecto/feature/Carrito/controllers/put/ModificarCantidadController.java
@@ -0,0 +1,20 @@
+package MetodologiaDeSistema.Proyecto.feature.Carrito.controllers.put;
+
+import MetodologiaDeSistema.Proyecto.feature.Carrito.Service.Impl.CarritoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/carrito")
+public class ModificarCantidadController {
+
+    @Autowired
+    private CarritoService carritoService;
+
+    @PutMapping("/items/{itemId}")
+    public void modificarCantidad(@PathVariable Long itemId,
+                                  @RequestParam Integer cantidad) {
+
+        carritoService.modificarCantidad(itemId, cantidad);
+    }
+}
