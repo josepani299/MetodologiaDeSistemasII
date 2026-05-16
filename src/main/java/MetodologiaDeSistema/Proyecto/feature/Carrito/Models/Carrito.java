3b11502e9dc7600fafd22ea64f5a3b8be1a8d0d8
RamiroHH <ramiroh.dev@gmail.com>
2026-05-16 01:59:19 -0300

    Update Carrito.java

--- src/main/java/MetodologiaDeSistema/Proyecto/feature/Carrito/Models/Carrito.java
+++ src/main/java/MetodologiaDeSistema/Proyecto/feature/Carrito/Models/Carrito.java
@@ -1,5 +1,6 @@
 package MetodologiaDeSistema.Proyecto.feature.Carrito.Models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +19,7 @@ public class Carrito {
 
     private Double total = 0.0;
 
+    @JsonManagedReference
     @OneToMany(mappedBy = "carrito", cascade = CascadeType.ALL)
     private List<CarritoItem> items = new ArrayList<>();
 }
