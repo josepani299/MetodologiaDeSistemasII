3a5abb45678685ca000c6fa30ab4bedb4daf3b90
EfrainCruzTurrin <efraincruzturrin@gmail.com>
2026-07-05 15:26:07 -0300

    Bug: No se agregaba las cosas al carrito

--- src/main/java/MetodologiaDeSistema/Proyecto/feature/Carrito/Dtos/Request/AgregarProductoRequestDto.java
+++ src/main/java/MetodologiaDeSistema/Proyecto/feature/Carrito/Dtos/Request/AgregarProductoRequestDto.java
@@ -2,9 +2,11 @@ package MetodologiaDeSistema.Proyecto.feature.Carrito.Dtos.Request;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class AgregarProductoRequestDto {
 
     private Long productoId;
